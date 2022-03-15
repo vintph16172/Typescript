258b6054ffb732136357b22c142ad654766e4cd1
vintph16172 <vintph16172@fpt.edu.vn>
2022-03-15 16:14:59 +0700

    update

---
+++ src/main.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+
+const myName: string = "Vĩ"
+const myStatus: boolean = true
+const myAge: number = 20
+const products: {id:number, name:string} = {
+  id: 1,
+  name: "a" 
+}
+
+type ShowProps = {
+  name : string
+}
+function show(a:ShowProps):any{
+  console.log(a.name);
+  return null
+}
+
+function Show(a: ShowProps):any{
+  console.log(a.name);
+  
+  return null
+}
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {myName}</h1>
+    <div>{myStatus ? "1" :"2"}</div>
+    <div>{myAge}</div>
+    <div>{products.name}</div>
+    <div className="">{show({name: myName})}</div>
+    <div className=""><Show name={myName} /></div>
+
+  </div>
+
+  ,document.querySelector("#root")
+)
